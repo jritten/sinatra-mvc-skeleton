67b5bc44070ae855c53cf4d4f46e2571d215bfd8
jritten <jritten@umich.edu>
2017-08-23 08:11:06 -0700

    CRUD episode 3

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,3 +1,51 @@
 get '/' do 
   "Hello, World"
 end
+
+# CONTACTS RESTful actions
+
+# index
+get '/contacts' do
+  # "This is the contacts index action"
+  @contacts = Contact.all 
+  @contacts.map{ |contact| contact.email }.to_s
+  erb :'/contacts/index'
+end
+
+# new
+get '/contacts/new' do
+  "This is the contacts new action"
+end
+
+# create
+post '/contacts' do
+  "This is the contacts create action"
+end
+
+# show
+get '/contacts/:id' do
+  # "This is the contacts show action"
+  # puts params
+  @contact = Contact.find(params[:id])
+  # @contact.email
+  erb :'/contacts/show'
+end
+
+# edit
+post '/contacts/:id/edit' do
+  "This is the contacts edit action"
+end
+
+# update
+patch '/contacts/:id' do
+  "This is the contacts update action"
+end
+
+put '/contacts/:id' do
+  "This is the contacts update action"
+end
+
+# delete
+delete '/contacts/:id' do
+  "This is the contacts delete action"
+end
